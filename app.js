6cb56bbb596b715e71ded6e9a3e7e96a76c41d32
Russ Perry <rperry1886@gmail.com>
2019-03-30 20:15:50 -0400

    Create a function to draw circles

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let backgroundTheme = "#111111";
 let themeColor = "#bada55";
 
 //Ball Variables
-let ballWidth = 10;
+let ballWidth = 8;
 let ballHeight = 10;
 let ballX = 50;
 let ballSpeedX = 5;
@@ -21,23 +21,28 @@ window.onload = () => {
 //Draws the pieces to the game
 function drawAssets() {
   //Canvas
-  drawShape(0, 0, canvas.width, canvas.height, backgroundTheme);
+  drawRectangle(0, 0, canvas.width, canvas.height, backgroundTheme);
 
   //Ball
-  canvasContext.fillStyle = themeColor;
-  canvasContext.beginPath();
-  //Arc Paramters (xPos, yPos, radius (size), angle, radian, clockwise(T) || CounterClockwise(F))
-  canvasContext.arc(ballX, 100, 5, 0, Math.PI * 2, true);
-  canvasContext.fill();
+  drawCircle(ballX, 150, themeColor);
 
   //Left Paddle
-  drawShape(10, 210, 10, 100, themeColor);
+  drawRectangle(10, 210, 10, 100, themeColor);
 }
 
-//Template function to draw shapes
-function drawShape(x, y, width, height, color) {
+//Template function to draw rectangles/squares
+function drawRectangle(x, y, width, height, color) {
   canvasContext.fillStyle = color;
   canvasContext.fillRect(x, y, width, height);
+}
+
+//Tempate function to draw circles
+function drawCircle(x, y, color) {
+  canvasContext.fillStyle = color;
+  canvasContext.beginPath();
+  //Arc Paramters (xPos, yPos, radius (size), angle, radian, clockwise(T) || CounterClockwise(F))
+  canvasContext.arc(x, y, ballWidth, 0, Math.PI * 2, true);
+  canvasContext.fill();
 }
 
 //Moves the movable pieces to the game
